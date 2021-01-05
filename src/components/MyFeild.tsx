4480449ae4910dd5d64507d67355943bf410c706
Nujood <shawar.nujood@gmail.com>
2021-01-05 21:08:54 +0200

    remove unused files

--- src/components/MyFeild.tsx
+++ src/components/MyFeild.tsx
@@ -3,7 +3,6 @@ import { FieldProps } from 'formik'
 import { TextField, TextFieldProps } from '@material-ui/core'
 
 // adding TextFieldProps declare the types of the field props byDefault for <TextField>
-
 export const MyField: React.FC<FieldProps & TextFieldProps> = ({label, placeholder, field}) => (
   <TextField
     color="primary"
